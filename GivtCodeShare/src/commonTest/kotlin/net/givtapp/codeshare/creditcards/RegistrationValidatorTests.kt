e5ed8194fbc1e4143999528f82ca5c20ab14edf5
Mike Pattyn <mike@givtapp.net>
2021-10-14 23:39:08 +0200

    test phonenumber validation

--- GivtCodeShare/src/commonTest/kotlin/net/givtapp/codeshare/creditcards/RegistrationValidatorTests.kt
+++ GivtCodeShare/src/commonTest/kotlin/net/givtapp/codeshare/creditcards/RegistrationValidatorTests.kt
@@ -2,16 +2,21 @@ package net.givtapp.codeshare.creditcards
 
 import net.givtapp.codeshare.registration.RegistrationValidator
 import kotlin.test.Test
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class RegistrationValidatorTests {
     private val _registrationValidator = RegistrationValidator()
 
     @Test
-    fun ensureValidationWorks() {
-        _registrationValidator.emailAddress = "testen@givtapp.net"
-        _registrationValidator.phoneNumber = "+32495430410"
+    fun ensurePasswordValidationWorks() {
+        _registrationValidator.password = "test123"
+        assertFalse { _registrationValidator.isValidPassword }
+        _registrationValidator.password = "Tt123"
+        assertFalse { _registrationValidator.isValidPassword }
+        _registrationValidator.password = "testTTT"
+        assertFalse { _registrationValidator.isValidPassword }
         _registrationValidator.password = "Test123"
-        assertTrue { _registrationValidator.isValidRegistrationData }
+        assertTrue { _registrationValidator.isValidPassword }
     }
 }
