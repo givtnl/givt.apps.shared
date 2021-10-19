a945868d5f3d0dc333dc4bf1d66ae90b3bec7f60
Mike Pattyn <mike@givtapp.net>
2021-10-19 13:01:38 +0200

    Make properties public

--- GivtCodeShare/src/commonMain/kotlin/net/givtapp/codeshare/infrastructure/models/YearMonth.kt
+++ GivtCodeShare/src/commonMain/kotlin/net/givtapp/codeshare/infrastructure/models/YearMonth.kt
@@ -12,8 +12,8 @@ class YearMonth {
         rawValueChangedListener.onValueChanged(oldValue, newValue)
     }
 
-    internal var month: Int? = null
-    internal var year: Int? = null
+    var month: Int? = null
+    var year: Int? = null
 
     val formatted: String
         get() = "${month.toString().padStart(2, '0')}/${year.toString().substring(2)}"
