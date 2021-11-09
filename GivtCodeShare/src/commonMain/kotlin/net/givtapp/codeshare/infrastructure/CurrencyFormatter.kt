25d3ccbcadf5e782601baebecd599082b8d5aab7
Mike Pattyn <mike@givtapp.net>
2021-11-09 12:59:41 +0100

    handle locale within the client apps

--- GivtCodeShare/src/commonMain/kotlin/net/givtapp/codeshare/infrastructure/CurrencyFormatter.kt
+++ GivtCodeShare/src/commonMain/kotlin/net/givtapp/codeshare/infrastructure/CurrencyFormatter.kt
@@ -1,38 +1,26 @@
 package net.givtapp.codeshare.infrastructure
 
-import kotlin.native.concurrent.ThreadLocal
-
-@ThreadLocal // Annotation makes sure this object can be used as a threadsafe singleton.
 object CurrencyFormatter {
-    var userLocale: CustomLocale? = null
-        get() {
-                if (field == null)
-                    throw CurrencyFormatterException("UserLocale cannot be null!")
-                else
-                    return field!!
-            }
+    fun getLocalFormat(value: Float, decimals: Boolean = true, localeString: String): String {
+        val userLocale = CustomLocale(localeString)
 
-    fun setUserLocale(locale: String) {
-        this.userLocale = CustomLocale(locale)
-    }
-
-    fun getLocalFormat(value: Float, decimals: Boolean = true): String {
         if (value.isNaN())
             throw CurrencyFormatterException("Value isNaN")
-        var formatted = DecimalFormat().format(value.toDouble(), if (decimals) 2 else 0, userLocale!!)
-        formatted = reverseCurrencyAndAmountOrder(formatted)
+        var formatted = DecimalFormat().format(value.toDouble(), if (decimals) 2 else 0, userLocale)
+        formatted = reverseCurrencyAndAmountOrder(formatted, userLocale)
         return formatted
     }
 
-    fun getCurrencySymbol(): String {
-        return DecimalFormat().getCurrencySymbol(userLocale!!).toString()
+    fun getCurrencySymbol(localeString: String): String {
+        val userLocale = CustomLocale(localeString)
+        return DecimalFormat().getCurrencySymbol(userLocale).toString()
     }
 
-    private fun reverseCurrencyAndAmountOrder(originalValue: String): String {
+    private fun reverseCurrencyAndAmountOrder(originalValue: String, userLocale: CustomLocale): String {
         // We ALWAYS want the net.givtapp.codeshare.currency symbols for Belgium and Germany at beginning
-        if (arrayListOf("BE", "DE").contains(userLocale!!.country)) {
+        if (arrayListOf("BE", "DE").contains(userLocale.country)) {
             var formattedValue = originalValue
-            formattedValue = formattedValue.replace(getCurrencySymbol(), "")
+            formattedValue = formattedValue.replace(getCurrencySymbol(userLocale.localeString), "")
             formattedValue = formattedValue.removeSuffix(Typography.nbsp.toString())
             formattedValue = "€${Typography.nbsp}$formattedValue"
             return formattedValue
