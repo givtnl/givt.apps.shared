ff018901f2adfcb3cf515f997c597069c0ab7e57
Mike Pattyn <mike@givtapp.net>
2021-12-03 16:23:45 +0100

    added first and last name properties to register command

--- GivtCodeShare/src/commonMain/kotlin/net/givtapp/codeshare/api/User/Register/RegisterUserCommandBody.kt
+++ GivtCodeShare/src/commonMain/kotlin/net/givtapp/codeshare/api/User/Register/RegisterUserCommandBody.kt
@@ -3,11 +3,13 @@ package net.givtapp.codeshare.api.User.Register
 import kotlinx.serialization.Serializable
 
 @Serializable
-class RegisterUserCommandBody(
+open class RegisterUserCommandBody(
     var userId: String,
     var email: String,
     var phoneNumber: String,
     var password: String,
     var appLanguage: String,
     var deviceOS: Int,
-    var country: String)
+    var country: String,
+    var firstName: String,
+    var lastName: String)
