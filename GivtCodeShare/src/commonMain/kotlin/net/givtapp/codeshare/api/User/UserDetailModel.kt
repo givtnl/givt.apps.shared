232df95ac15c081279aa76d95e86567410792c7e
Michiel Verkest <michiel@Michiels-MacBook-Pro.local>
2022-02-09 15:51:36 +0100

    add properties bc jsonIgnore doesn't seem to work and we want to go fast

--- GivtCodeShare/src/commonMain/kotlin/net/givtapp/codeshare/api/User/UserDetailModel.kt
+++ GivtCodeShare/src/commonMain/kotlin/net/givtapp/codeshare/api/User/UserDetailModel.kt
@@ -8,4 +8,17 @@ class UserDetailModel(
     val Email: String,
     val Country: String,
     val Language: String,
-    val PhoneNumber: String)
+    val PhoneNumber: String,
+    val Iban: String? = null,
+    val SortCode: String? = null,
+    val AccountNumber: String? = null,
+    val FirstName: String? = null,
+    val LastName: String? = null,
+    val IsDisabled: Boolean? = null,
+    val City: String? = null,
+    val Address: String? = null,
+    val PostalCode: String? = null,
+    val DetailLineOne: String? = null,
+    val DetailLineTwo: String? = null,
+    val DetailLineThree: String? = null
+)
